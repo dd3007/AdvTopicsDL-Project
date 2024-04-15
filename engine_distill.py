dec8ed09bcb4f38998ba369efcebad78920b34d0
Maximo Librandi <maximolibrandi@hotmail.com>
2024-04-15 11:52:17 -0400

    Refactor engine_distill.py

--- engine_distill.py
+++ engine_distill.py
@@ -3,21 +3,22 @@ import sys
 from typing import Iterable
 
 import torch
-import torch.nn as nn
 
 import util.misc as misc
 import util.lr_sched as lr_sched
-
 
 def train_one_epoch(model: torch.nn.Module, model_teacher: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
                     device: torch.device, epoch: int, loss_scaler,
                     log_writer=None,
                     args=None):
+    
     model.train(True)
     model_teacher.eval()
+    
     metric_logger = misc.MetricLogger(delimiter="  ")
     metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    
     header = 'Epoch: [{}]'.format(epoch)
     print_freq = 20
 
@@ -28,33 +29,33 @@ def train_one_epoch(model: torch.nn.Module, model_teacher: torch.nn.Module,
     if log_writer is not None:
         print('log_dir: {}'.format(log_writer.log_dir))
     print(len(data_loader))
+    
     for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
 
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+
         if isinstance(samples, list):
             imgs = samples[0].to(device, non_blocking=True)
             heatmaps = samples[1].to(device, non_blocking=True)
+
         else:
             imgs = samples.to(device, non_blocking=True)
             heatmaps = None
 
-
-
         with torch.cuda.amp.autocast():
+            
             with torch.no_grad():
                 latents_teacher, mask, ids_restore, ids_keep = \
                     model_teacher.module.forward_encoder_customized(imgs, args.mask_ratio)
-                teacher_prediction = model_teacher.module.forward_decoder(latents_teacher[-1],
-                                                                            ids_restore)  
+                teacher_prediction = model_teacher.module.forward_decoder(latents_teacher[-1], ids_restore)  
+            
             loss, loss_distillation_embedding, _, _ = model(imgs, ids_keep, ids_restore, mask, teacher_prediction,
                                                             args.target_sum_weights, latents_teacher)
 
             loss_value = loss.item()
             for loss_k, loss_v in loss_distillation_embedding.items():
                 loss += loss_v
-
-
 
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
@@ -63,6 +64,7 @@ def train_one_epoch(model: torch.nn.Module, model_teacher: torch.nn.Module,
         loss /= accum_iter
         loss_scaler(loss, optimizer, parameters=model.parameters(),
                     update_grad=(data_iter_step + 1) % accum_iter == 0)
+
         if (data_iter_step + 1) % accum_iter == 0:
             optimizer.zero_grad()
 
@@ -91,7 +93,6 @@ def train_one_epoch(model: torch.nn.Module, model_teacher: torch.nn.Module,
                 log_writer.add_scalar('train_loss_total', loss_total_value_reduce, epoch_1000x)
                 for key, value in loss_distillation_embedding.items():
                     log_writer.add_scalar(f'distillation_loss/{key}', value, epoch_1000x)
-
 
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
