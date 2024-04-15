9b9f6056373a0615489c2a8345e6a54eaa253c5c
Manuel Paez <79882298+mannypaeza@users.noreply.github.com>
2024-04-15 14:59:49 -0400

    pretrain_v2.sh

--- pretrain_v2.sh
+++ pretrain_v2.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-module load slurm
-module load cuda
-module load cudnn nccl
-
-NUMBER_OF_GPUS=2
-
 EXP_NAME=distill_model
+GPUS=8
 SAVE_DIR1="/mnt/home/mpaez/ceph/distill/${EXP_NAME}_ver1/"
 MODEL_NAME='latest.pth'
 CHESTXRAY_DIR='/mnt/home/mpaez/ceph/chestxray'
 CHEXPERTSMALL='/mnt/home/mpaez/ceph/CheXpert-v1.0-small'
 
-torchrun --standalone --nnodes 1 --nproc_per_node=$NUMBER_OF_GPUS \
+
+OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=${GPUS} \
     --use_env main_distill.py \
     --output_dir ${SAVE_DIR1} \
     --log_dir ${SAVE_DIR1} \
     --batch_size 128 \
     --accum_iter 4 \
-    --model mae_vit_tiny_patch16_dec512d8b \
+    --model mae_vit_base_patch16_dec512d8b \
     --model_teacher mae_vit_large_patch16_dec512d8b \
     --mask_ratio 0.75 \
     --epochs 100 \
     --blr 1.5e-4 --weight_decay 0.05 \
-    --data_path ${CHEXPERTSMALL} \
+    --data_path ${IMAGENET_DIR} \
     --teacher_model_path '/mnt/home/mpaez/ceph/adp_model/mae_visualize_vit_large.pth' \
     --student_reconstruction_target 'original_img' \
     --aligned_blks_indices 8 \
