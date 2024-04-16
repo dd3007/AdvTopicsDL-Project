4b8bc8065a3adfd56884183f27c5d3c128e5b1ec
Maximo Librandi <maximolibrandi@hotmail.com>
2024-04-16 19:40:07 -0400

    Update distill script to teacher-model=ViT-Base pretrained on medical data

--- distill.sh
+++ distill.sh
@@ -12,14 +12,14 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=${GPUS} \
     --batch_size 32 \
     --accum_iter 4 \
     --model mae_vit_small_patch16_dec512d8b \
-    --model_teacher mae_vit_large_patch16_dec512d8b \
+    --model_teacher mae_vit_base_patch16_dec512d8b \
     --mask_ratio 0.75 \
     --epochs 100 \
     --blr 1.5e-4 --weight_decay 0.05 \
     --data_path ${IMAGENET_DIR} \
-    --teacher_model_path 'mae_visualize_vit_large.pth' \
+    --teacher_model_path 'vit-b_CXR_0.5M_mae.pth' \
     --student_reconstruction_target 'original_img' \
     --aligned_blks_indices 8 \
-    --teacher_aligned_blks_indices 17 \
+    --teacher_aligned_blks_indices 8 \
     --embedding_distillation_func L1 \
-    --aligned_feature_projection_dim 384 1024
+    --aligned_feature_projection_dim 384 768
