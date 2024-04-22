36573d9b3f79655917598c23718725cb3015febd
Maximo Librandi <maximolibrandi@hotmail.com>
2024-04-22 15:23:17 -0400

    Update fnetune script

--- med_finetune.sh
+++ med_finetune.sh
@@ -9,14 +9,14 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch \
     --log_dir ${SAVE_DIR} \
     --batch_size 32 \
     --finetune "tiny_mae_pretrained.pth" \
-    --epochs 75 \
+    --epochs 70 \
     --blr 2.5e-4 --layer_decay 0.55 --weight_decay 0.05 \
     --model vit_tiny_patch16 \
     --warmup_epochs 5 \
     --drop_path 0.2 --mixup 0 --cutmix 0 --reprob 0 --vit_dropout_rate 0 \
     --num_workers 4 \
     --nb_classes 14 \
-    --eval_interval 2 \
+    --eval_interval 10 \
     --min_lr 1e-5 \
     --build_timm_transform \
     --aa 'rand-m6-mstd0.5-inc1'
