ab418acbdc735a032edc4197d4c4f31f48a96950
Manuel Paez <79882298+mannypaeza@users.noreply.github.com>
2024-04-25 12:19:22 -0400

    fixing slurm files

--- med_finetune_slurm.sh
+++ med_finetune_slurm.sh
@@ -1,10 +1,24 @@
+#!/bin/bash -1 
+
+#SBATCH -p gpu
+#SBATCH -N 2
+#SBATCH -C a100, ib
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-gpu=8
+
 EXP_NAME=finetuned_tiny_model
 SAVE_DIR="./work_dirs/${EXP_NAME}_e1/"
-GPUS=4
 
-OMP_NUM_THREADS=1 python -m torch.distributed.launch \
-    --nproc_per_node=${GPUS} \
-    --use_env main_med_finetune.py \
+master_node=$SLURMD_NODENAME
+
+srun python -m 'which torchrun' \
+    --nnodes $SLURM_JOB_NUM_NODES \
+    --nproc_per_node $SLURM_GPUS_PER_NODE \
+    --rdzv_id $SLURM_JOB_ID 
+    --rdzv_endpoint $master_node:29500 \
+    main_med_finetune.py \
     --output_dir ${SAVE_DIR} \
     --log_dir ${SAVE_DIR} \
     --batch_size 32 \
