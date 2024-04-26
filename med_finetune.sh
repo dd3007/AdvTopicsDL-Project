e8462da5c5d85492e83c6ef987d0984a522e48bb
Maximo Librandi <maximolibrandi@hotmail.com>
2024-04-25 23:14:05 -0400

    Update script files

--- med_finetune.sh
+++ med_finetune.sh
@@ -7,16 +7,16 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch \
     --use_env main_med_finetune.py \
     --output_dir ${SAVE_DIR} \
     --log_dir ${SAVE_DIR} \
-    --batch_size 100 \
+    --batch_size 32 \
     --model vit_tiny_patch16 \
     --finetune "tiny_mae_pretrained.pth" \
     --dataset chestxray14 \
     --nb_classes 14 \
-    --epochs 70 \
+    --epochs 100 \
     --blr 2.5e-4 --layer_decay 0.55 --weight_decay 0.05 \
     --warmup_epochs 5 \
     --drop_path 0.2 \
     --mixup 0 --cutmix 0 \
     --vit_dropout_rate 0 \
     --num_workers 4 \
-    --eval_interval 1 \
+    --eval_interval 10 \
