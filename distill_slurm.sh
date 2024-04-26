30a09e94de56632ea33322e9111d1cae007e3942
Manuel Paez <79882298+mannypaeza@users.noreply.github.com>
2024-04-25 22:12:15 -0400

    python

--- distill_slurm.sh
+++ distill_slurm.sh
@@ -9,7 +9,7 @@
 EXP_NAME=distilled_tiny_model
 SAVE_DIR="/mnt/home/mpaez/ceph/adp_model/distill/${EXP_NAME}_e1/"
 
-srun python -m 'which torchrun' \
+srun python 'which torchrun' \
     --nnodes $SLURM_JOB_NUM_NODES \
     --nproc_per_node $SLURM_GPUS_PER_NODE \
     --rdzv_id $SLURM_JOB_ID \
