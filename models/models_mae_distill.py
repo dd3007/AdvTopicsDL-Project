ec5a49788cafaf44cb6694464fd1dcd86fb818a7
Manuel Paez <79882298+mannypaeza@users.noreply.github.com>
2024-04-28 11:19:09 -0400

    fix

--- models/models_mae_distill.py
+++ models/models_mae_distill.py
@@ -3,7 +3,7 @@ from functools import partial
 import torch
 import torch.nn as nn
 
-from timm.models.vision_transformer import PatchEmbed, Mlp, Attention
+from timm.models.vision_transformer import PatchEmbed, Mlp, Block
 # from attention import Block 
 
 from util.pos_embed import get_2d_sincos_pos_embed
