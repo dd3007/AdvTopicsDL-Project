2dc9b060a30c743ebaa4382782d20a47194981ee
Maximo Librandi <maximolibrandi@hotmail.com>
2024-04-29 15:49:25 -0400

    Add script to evaluate

---
+++ med_evaluate.sh
@@ -0,0 +1,23 @@
+EXP_NAME=finetuned_tiny_model
+SAVE_DIR="./work_dirs/${EXP_NAME}_e1/"
+GPUS=4
+
+OMP_NUM_THREADS=1 python -m torch.distributed.launch \
+    --nproc_per_node=${GPUS} \
+    --use_env main_med_finetune.py \
+    --output_dir ${SAVE_DIR} \
+    --log_dir ${SAVE_DIR} \
+    --batch_size 32 \
+    --model vit_tiny_patch16 \
+    --dataset chexpert \
+    --nb_classes 5 \
+    --epochs 100 \
+    --blr 2.5e-4 --layer_decay 0.55 --weight_decay 0.05 \
+    --warmup_epochs 5 \
+    --drop_path 0.2 \
+    --mixup 0 --cutmix 0 \
+    --vit_dropout_rate 0 \
+    --num_workers 4 \
+    --eval_interval 10 \
+    --resume "finetune_chexpert_base_tiny_100epochs_4gpus.pth" \
+    --eval
